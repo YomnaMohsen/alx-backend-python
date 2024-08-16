068d9ba6575e547ed4b9d81f3e76fa721f16e522
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-16 22:33:10 +0300

    style

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -4,6 +4,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """takes float as arg and return function that 
+    multiples 2 floats"""
     def mul(x):
+        """multipy 2 floats"""
         return multiplier * x
     return mul

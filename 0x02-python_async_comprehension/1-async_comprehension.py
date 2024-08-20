339615651d53bc15203465663f00815177e718da
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-08-20 19:47:54 +0300

    fix t 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -2,10 +2,10 @@
 "async comprehension module"
 import random
 import asyncio
-from typing import Iterator
+from typing import List
 async_gen = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Iterator[float]:
+async def async_comprehension() -> List[float]:
     result = [item async for item in async_gen()]
     return result

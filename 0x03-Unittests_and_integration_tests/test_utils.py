4001ca20a81b5e7f3ba480487695d1b45cc6c0c1
YomnaMohsen <ymn_mohsen@yahoo.com>
2024-11-03 21:41:41 +0200

    t 0

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,7 @@
-import unittest
+#!/usr/bin/env python3
 """test module"""
+
+import unittest
 from parameterized import parameterized
 from utils import access_nested_map
 
@@ -9,9 +11,9 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({"a": 1}, "a", 1),
-        ({"a": {"b": 2}}, ("a",),{"b": 2} ),
-        ({"a": {"b": 2}}, ("a", "b"), 2)  
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, map, path, value):
         """test function access_nested_map"""
-        self.assertEqual(access_nested_map(map,path), value)
+        self.assertEqual(access_nested_map(map, path), value)
